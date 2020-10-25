7896d13366b932babddc756b59764fd0854409dd
Mimi Netto <michnetto249@gmail.com>
2020-10-25 16:43:40 -0400

    api changes

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module WeddingAppApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
